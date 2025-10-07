d985af3c8a537ec7e4f607dbe8a3b22d530893c8
Thanigaiyarasup <thanigaiyarasu7@gmail.com>
2025-10-07 14:10:38 +0530

    first commit

---
+++ frontend/src/components/layouts/Footer.js
@@ -0,0 +1,9 @@
+export  default function Footer (){
+    return (
+        <footer className="py-1">
+            <p className="text-center text-dark mt-1">
+                © 2025 Your Website. All rights reserved.
+            </p>
+        </footer>
+    )
+}
